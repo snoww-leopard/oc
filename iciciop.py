5727c51d423fec1aed91e1d5961d4769b689094b
snoww leoopard <snoww.leopard@snowws-MacBook-Pro.local>
2025-08-25 23:48:36 +0530

    Modified code for payload array

--- iciciop.py
+++ iciciop.py
@@ -12,27 +12,32 @@ session_key = op_request['session_key']
 secret_key= sys.argv[2]
 print(secret_key)
 appkey= sys.argv[1]
-time_stamp = datetime.now(timezone.utc).isoformat()[:19] + '.000Z'
+
 
 customerDetail_url = op_request['customerDetail_url']
 session_token = get_session_token(customerDetail_url, appkey, session_key)
+count = 0
+
+for item in op_request['optionchain_method']:
+    #print(f'{op_request['optionchain']['payload']}')
+    count += 1
+    print(f"============================================={count}")
+    url = op_request["option_chain_url"]
+    time_stamp = datetime.now(timezone.utc).isoformat()[:19] + '.000Z'
+    #FutureUrl = "https://api.icicidirect.com/breezeapi/api/v1/quotes"
+
+    payload = json.dumps(item['payload'], separators=(',', ':'))
+    checksum = hashlib.sha256((time_stamp+payload+secret_key).encode("utf-8")).hexdigest()
+
+    result = []
+    response_json = get_optionchain(url, appkey, session_token, payload, checksum)
+
+    option_chain = response_json.get("Success", [])
+
+    strikediff = item['strikediff'] # diffrence between first and second strikes
+    lot_size = item['lot_size'] # lot size for ICICI Bank options
 
 
-#print(f'{op_request['optionchain']['payload']}')
-url = op_request['optionchain_method']['url']
-#FutureUrl = "https://api.icicidirect.com/breezeapi/api/v1/quotes"
+    result = process_option_data(option_chain, strikediff, lot_size)
 
-payload = json.dumps(op_request['optionchain_method']['payload'], separators=(',', ':'))
-checksum = hashlib.sha256((time_stamp+payload+secret_key).encode("utf-8")).hexdigest()
-
-result = []
-response_json = get_optionchain(url, appkey, session_token, payload, checksum)
-
-option_chain = response_json.get("Success", [])
-strikediff = op_request['optionchain_method']['strikediff'] # diffrence between first and second strikes
-lot_size = op_request['optionchain_method']['lot_size'] # lot size for ICICI Bank options
-
-
-result = process_option_data(option_chain, strikediff, lot_size)
-
-print(result)
+    #print(result)
